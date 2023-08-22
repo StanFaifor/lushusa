a083afd2654533e2ebd37aa2ed63df4ee70fd194
StanFaifor <119562928+StanFaifor@users.noreply.github.com>
2023-08-22 13:39:38 -0700

    top Sub Header added

---
+++ index.js
@@ -0,0 +1,11 @@
+// Top Sub Header
+
+function togglePopDownContainer() {
+  const containerOn = document.getElementById("popDownContainer");
+  containerOn.style.display = "block";
+}
+
+function hidePopDownContainer() {
+  const containerOn = document.getElementById("popDownContainer");
+  containerOn.style.display = "none";
+}
